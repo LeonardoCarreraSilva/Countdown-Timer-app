126b79a6c8260bbad841e46780fa6a96ffcb6fe4
Leonardo Carrera da Silva <59487403+LeonardoCarreraSilva@users.noreply.github.com>
2024-10-22 06:56:10 -0300

    add home

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,13 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import moment from 'moment';
+import { HomeComponent } from './pages/home/home.component';
 
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, HomeComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })

ecbbfbaf77f1013d582ffc24a675ace235bb2969
Leonardo Carrera da Silva <59487403+LeonardoCarreraSilva@users.noreply.github.com>
2024-10-23 06:22:31 -0300

    add material ui

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,13 +1,36 @@
 import { Component } from '@angular/core';
+import { FormBuilder, FormGroup, FormsModule, ReactiveFormsModule } from '@angular/forms';
 import moment from 'moment';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatIconModule } from '@angular/material/icon';
+import { MatInputModule } from '@angular/material/input';
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [],
+  imports: [FormsModule, ReactiveFormsModule, MatFormFieldModule, MatInputModule, MatIconModule],
   templateUrl: './home.component.html',
   styleUrl: './home.component.scss'
 })
 export class HomeComponent {
+  title: string = ''
+  date: string = ''
+  fg: FormGroup
+
+  constructor(private _fg: FormBuilder) {
+    this.fg = _fg.group({
+      title: '',
+      date: ''
+    })
+  }
+
+  setCard() {
+    if(this.fg.valid){
+      this.title = this.fg.value.title
+      this.date = moment(this.fg.value.date).format('DD/MM/YYYY HH:mm')
+    }
+
+  }
+
 
 }
